72fade896e8c2b197229cc470d6aee185176c2a2
felipe.dev <083felype@gmail.com>
2024-10-31 17:17:39 -0300

    feat: add filter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserDetailsComponent } from './shared/components/user-details/user-details.component';
+import { FilterComponent } from './shared/components/filter/filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserDetailsComponent],
+  imports: [UserDetailsComponent, FilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/components/filter/filter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatButtonModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.scss'
+})
+export class FilterComponent {
+  foods: any = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
+}
