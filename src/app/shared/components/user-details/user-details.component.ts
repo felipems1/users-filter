f511e6f2e59f181b5e0789f0bd24f7e0c04c1e8c
felipe.dev <083felype@gmail.com>
2024-11-02 18:04:38 -0300

    feat: create pipe to format phone numbers

--- src/app/shared/components/user-details/user-details.component.ts
+++ src/app/shared/components/user-details/user-details.component.ts
@@ -3,11 +3,12 @@ import { UserDetailInfoComponent } from './components/user-detail-info/user-deta
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { IUser } from '../../interfaces/user/user.interface';
+import { PhonePipe } from '../../pipes/phone.pipe';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [UserDetailInfoComponent, MatListModule, MatDividerModule],
+  imports: [UserDetailInfoComponent, MatListModule, MatDividerModule, PhonePipe],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
