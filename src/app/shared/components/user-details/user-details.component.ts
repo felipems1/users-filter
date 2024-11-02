fdd7aabf17b1bd9ed6a93c935fb28582d72e0508
felipe.dev <083felype@gmail.com>
2024-11-02 18:19:56 -0300

    feat: create pipe to format address

--- src/app/shared/components/user-details/user-details.component.ts
+++ src/app/shared/components/user-details/user-details.component.ts
@@ -4,11 +4,12 @@ import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { IUser } from '../../interfaces/user/user.interface';
 import { PhonePipe } from '../../pipes/phone.pipe';
+import { AddressPipe } from '../../pipes/address.pipe';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [UserDetailInfoComponent, MatListModule, MatDividerModule, PhonePipe],
+  imports: [UserDetailInfoComponent, MatListModule, MatDividerModule, PhonePipe, AddressPipe],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
