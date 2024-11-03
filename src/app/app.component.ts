c2eac1a44fdfd6bfd4ea4a2f469f27e7d67205be
felipe.dev <083felype@gmail.com>
2024-11-03 12:05:32 -0300

    feat: add name filter to user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { UserDetailsComponent } from './shared/components/user-details/user-details.component';
 import { FilterComponent } from './shared/components/filter/filter.component';
 import { UsersListComponent } from './shared/components/users-list/users-list.component';
 import { IUser } from './shared/interfaces/user/user.interface';
 import { UsersList } from './mocks/users-list';
+import { IFilterOptions } from './shared/interfaces/filter-options.inteface';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,45 @@ import { UsersList } from './mocks/users-list';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public readonly userSelected = signal<IUser>({} as IUser);
   public readonly showUserDetails = signal<boolean>(false);
-  public readonly users = signal<IUser[]>(UsersList);
+  public readonly usersList = signal<IUser[]>([]);
+  public readonly usersListFiltered = signal<IUser[]>([]);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.usersList.set(UsersList);
+      this.usersListFiltered.set(this.usersList());
+    }, 1000);
+  }
 
   onUserSelected(user: IUser): void {
     this.userSelected.set(user);
     this.showUserDetails.set(true);
+  }
+
+  onFilter(filterOptions: IFilterOptions): void {
+    this.usersListFiltered.set(this.filterUsersList(filterOptions, this.usersList()));
+  }
+
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
+    return filteredList;
+  }
+  
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPED = name === undefined;
+
+    if (NAME_NOT_TYPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.nome.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+
+    return filteredList;
   }
 }
--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { IFilterOptions } from '../../interfaces/filter-options.inteface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatButtonModule, FormsModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
-  foods: any = [
-    { value: 'steak-0', viewValue: 'Steak' },
-    { value: 'pizza-1', viewValue: 'Pizza' },
-    { value: 'tacos-2', viewValue: 'Tacos' },
+  public readonly filterOptions: IFilterOptions = {
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined,
+  };
+
+  public readonly statusList = [
+    { description: 'Ativo', value: true },
+    { description: 'Inativo', value: false },
   ];
+
+  public readonly onFilterEmit = output<IFilterOptions>({ alias: 'onFilter' });
+
+  onFilter(): void {
+    this.onFilterEmit.emit(this.filterOptions);
+  };
 }
